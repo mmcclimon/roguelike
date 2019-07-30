46e85d98e841a9494a80c98d44504a17eee924ff
Michael McClimon <michael@mcclimon.org>
2019-07-30 07:33:52 -0400

    Remove some dead code

--- srl/player.py
+++ srl/player.py
@@ -5,11 +5,6 @@ class Player(Drawable):
         super().__init__(x=x, y=y, glyph='@')
         self.trace = trace
 
-
-    # also move the cursor position to ourselves
-    def draw(self, ctx):
-        super().draw(ctx)
-
     # a player cannot collide with themselves
     def handle_collisions(self, ctx):
         pass
--- srl/screens/map_window.py
+++ srl/screens/map_window.py
@@ -4,12 +4,6 @@ from srl.screens.base_window import BaseWindow
 # This is a janky wrapper around curses.window, since it doesn't seem like I
 # can rebless the curses.window object into my class.
 class MapWindow(BaseWindow):
-    def __init__(self, window):
-        super().__init__(window)
-
-    def __getattr__(self, name):
-        return getattr(self.window, name)
-
     def draw(self, ctx, refresh=True):
         self.window.erase()
 
