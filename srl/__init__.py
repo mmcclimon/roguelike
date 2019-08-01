02e162bd379cba8dc5cd10d2f5d6837c11c6aa0b
Michael McClimon <michael@mcclimon.org>
2019-07-31 22:57:29 -0400

    Move some code into __init__.py

--- srl/__init__.py
+++ srl/__init__.py
@@ -1,2 +1,21 @@
 # __init__.py file for srl
-from srl.srl import SRL
+import curses
+import srl.context
+
+def run():
+    try:
+        res = curses.wrapper(_main)
+    except (KeyboardInterrupt):
+        pass
+
+    res.print()
+
+def _main(screen):
+    ctx = srl.context.Context(screen)
+
+    while ctx.is_running():
+        ctx.loop_once()
+
+    # When this is done, it'll set context.outcome
+    return ctx.generate_result()
+
--- srl/exceptions.py
+++
@@ -1,2 +0,0 @@
-class UserQuit(Exception):
-    pass
